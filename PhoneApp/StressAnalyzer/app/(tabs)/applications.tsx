0b440ffc650ccf5b81db2385145537906c966679
Mike Jensen <mike.jensen.at@gmail.com>
2024-05-03 13:34:57 +0200

    New screen: Apps
    Based on code found on the home page before. You can click on an application and get more daily info about the app.

---
+++ PhoneApp/StressAnalyzer/app/(tabs)/applications.tsx
@@ -0,0 +1,96 @@
+import { FlatList, Pressable, StyleSheet, useColorScheme } from 'react-native';
+import { Text, View } from '@/components/Themed';
+import { useState } from 'react';
+import { Image } from "react-native";
+import { Link } from 'expo-router';
+import Card from '@/components/Card';
+import { screenTimeData } from '@/constants/DummyData';
+import FontAwesome5 from '@expo/vector-icons/build/FontAwesome5';
+
+
+export default function ApplicationsScreen() {
+    const [date] = useState(new Date());
+
+    const renderAppsHeader = () => {  
+        return (
+          <View style={styles.flatlistItemContainer}>
+            <View style={{flex: 1, backgroundColor: 'transparent'}}></View>
+            <View style={{flex: 4, backgroundColor: 'transparent'}}></View>
+            <View style={styles.flatlistHeaderTextContainer}><Text style={{fontWeight: 'bold'}}>Avg. Stress</Text></View>
+            <View style={styles.flatlistHeaderTextContainer}><Text style={{fontWeight: 'bold'}}>Ref. Stress</Text></View>
+            <View style={styles.flatlistHeaderTextContainer}></View>
+          </View>
+        )
+      }
+
+    const renderAppsItem = ({item, index}: { item: any, index: number}) => (
+        <Link href={{pathname: "/screenTime", params: { date: date.getTime(), image: item.image, name: item.name }}} asChild>
+          <Pressable style={styles.flatlistItemContainer}>
+            <View style={{ flex: 1, backgroundColor: 'transparent', justifyContent: 'center'}}><Image source={item.image} style={styles.imageStyle} /></View>
+            <View style={{ flex: 4, backgroundColor: 'transparent', justifyContent: 'center'}}><Text>{item.name}</Text></View>
+            <View style={styles.flatlistItemTextContainer}><Text>{item.averageStress}</Text></View>
+            <View style={styles.flatlistItemTextContainer}><Text>{item.referenceStress}</Text></View>
+            <View style={styles.flatlistItemTextContainer}>
+              <FontAwesome5
+                name="arrow-right"
+                size={18}
+                color={'white'}
+                />
+            </View>
+          </Pressable>
+        </Link>
+      )
+
+
+    return (
+        <View style={{flex: 2}}>
+        <View style={styles.containerLabelsContainer}><Text style={styles.containerLabelsText}>Apps</Text></View>
+        <Card>
+          <FlatList
+            data={screenTimeData}
+            renderItem={renderAppsItem}
+            ListHeaderComponent={renderAppsHeader}
+            ListHeaderComponentStyle={styles.flatlistHeaderTextContainer}
+            stickyHeaderIndices={[0]}/>
+        </Card>
+      </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    flatlistItemContainer: {
+        flexDirection: 'row',
+        flex: 1,
+        backgroundColor: 'transparent',
+        margin: 4,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    flatlistItemTextContainer: {
+        flex: 3,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    flatlistHeaderTextContainer: {
+        flex: 3,
+        backgroundColor: '#3B3B3B',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 3,
+        paddingBottom: 3,
+    },
+    containerLabelsContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    containerLabelsText: {
+        fontSize: 30,
+        padding: 30,
+    },
+    imageStyle: {
+        width: 24,
+        height: 24,
+    },
+});
--- PhoneApp/StressAnalyzer/app/screenTime.tsx
+++ PhoneApp/StressAnalyzer/app/screenTime.tsx
@@ -1,33 +1,30 @@
-import { FlatList, Pressable, StyleSheet, useColorScheme, Image } from 'react-native';
+import { FlatList, Pressable, StyleSheet, useColorScheme, Image, TouchableOpacity } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { useLocalSearchParams } from 'expo-router';
-import { FontAwesome5 } from '@expo/vector-icons';
-import Colors from '@/constants/Colors';
-import { useState } from 'react';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { useEffect, useState } from 'react';
+import FontAwesome5 from '@expo/vector-icons/build/FontAwesome5';
 
 const getDaysInMonth = (year : number, month : number) => new Date(year, month, 0).getDate()
 
 export default function ScreenTimeScreen() {
   const colorScheme = useColorScheme() ?? 'light';
   const params = useLocalSearchParams<{ date: string, image: string, name: string }>();
-
-  const [date, setDate] = useState(new Date(parseInt(params.date)))
+  const navigation = useNavigation();
+  const [date] = useState(new Date(parseInt(params.date)))
   
+  const formatMinutes = (minutes: number) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
+  };
+
   const screenTimeData = Array.from({length: getDaysInMonth(date.getFullYear(), date.getMonth()+1)}, () => {
     return {
       "dailyAverage": Math.floor(Math.random() * 80),
       "appAverage": Math.floor(Math.random() * 80),
-      "usage": Math.floor(Math.random() * 400)
+      "usage": formatMinutes(Math.floor(Math.random()*100))
     }
   })
-
-  const showPreviousMonth = () => {
-    setDate(prev => new Date(prev.setMonth(prev.getMonth()-1)))
-  };
-
-  const showNextMonth = () => {
-    setDate(prev => new Date(prev.setMonth(prev.getMonth()+1)))
-  };
 
   const renderScreenTimeItem = ({item, index}: { item: any, index: number}) => {  
     return (
@@ -35,7 +32,7 @@ export default function ScreenTimeScreen() {
         <View style={styles.flatlistItemContainer}>
           <View style={styles.flatlistItemDate}>
             <Text style={styles.statsText}>Date</Text>
-            <Text>{index+1}/{date.getMonth()+1}</Text>
+            <Text>{screenTimeData.length - index}/{date.getMonth()+1}/{date.getFullYear()}</Text>
           </View>
           <View style={{ flex: 1, backgroundColor: 'transparent'}}></View>
           <View style={styles.flatlistItemDailyAverage}>
@@ -57,28 +54,19 @@ export default function ScreenTimeScreen() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.monthContainer}>
-        <Pressable onPress={showPreviousMonth}>
+      <View style={styles.appHeaderContainer}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
           <FontAwesome5
-            name="chevron-left"
-            size={24}
-            color={Colors[colorScheme].text}
-            style={styles.arrowButtons}
-          />
-        </Pressable>
-        <Text style={styles.monthText}>{date.toLocaleString('default', { month: 'long' })}</Text>
-        <Pressable onPress={showNextMonth}>
-          <FontAwesome5
-            name="chevron-right"
-            size={24}
-            color={Colors[colorScheme].text}
-            style={styles.arrowButtons}
-          />
-        </Pressable>
-      </View>
-      <View style={styles.appContainer}>
-        <Image source={Number(params.image)} style={styles.appImage} />
-        <Text style={styles.appName}>{params.name}</Text>
+              name="arrow-left"
+              size={24}
+              color={'white'}
+              />
+        </TouchableOpacity>
+        <View style={styles.appContainer}>
+          <Image source={Number(params.image)} style={styles.appImage} />
+          <Text style={styles.appName}>{params.name}</Text>
+        </View>
+        <View/>
       </View>
       <FlatList
         data={screenTimeData}
@@ -101,15 +89,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  arrowButtons: {
-    marginLeft: 20,
-    marginRight: 20,
+  appHeaderContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 12,
+    marginHorizontal: 12,
   },
   appContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 12,
+    justifyContent: 'space-between',
+    marginRight: 24,
   },
   appImage: {
     width: 64,
