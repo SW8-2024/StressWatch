a71dff0e7d5c035257cf842d7697abf94f4383da
Mike Jensen <mike.jensen.at@gmail.com>
2024-05-14 09:43:25 +0200

    Update applications.tsx

--- PhoneApp/StressAnalyzer/app/(tabs)/applications.tsx
+++ PhoneApp/StressAnalyzer/app/(tabs)/applications.tsx
@@ -9,126 +9,87 @@ import FontAwesome5 from '@expo/vector-icons/build/FontAwesome5';
 import TabContainer from '@/components/TabContainer';
 
 
-
-
-
-
-
-
 export default function ApplicationsScreen() {
-    const date = new Date();
+  const date = new Date();
 
-    const renderAppsHeader = () => {  
-        return (
-          <View style={styles.flatlistItemContainer}>
-            <View style={{flex: 1, backgroundColor: 'transparent'}}></View>
-            <View style={{flex: 4, backgroundColor: 'transparent'}}></View>
-            <View style={styles.flatlistHeaderTextContainer}><Text style={{fontWeight: 'bold'}}>Avg. Stress</Text></View>
-            <View style={styles.flatlistHeaderTextContainer}><Text style={{fontWeight: 'bold'}}>Ref. Stress</Text></View>
-            <View style={styles.flatlistHeaderTextContainer}></View>
-          </View>
-        )
-
-
-
-
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const renderAppsItem = ({item, index}: { item: any, index: number}) => (
-        <Link href={{pathname: "/screenTime", params: { date: date.getTime(), image: item.image, name: item.name }}} asChild>
-          <Pressable style={styles.flatlistItemContainer}>
-            <View style={{ flex: 1, backgroundColor: 'transparent', justifyContent: 'center'}}><Image source={item.image} style={styles.imageStyle} /></View>
-            <View style={{ flex: 4, backgroundColor: 'transparent', justifyContent: 'center'}}><Text>{item.name}</Text></View>
-            <View style={styles.flatlistItemTextContainer}><Text>{item.averageStress}</Text></View>
-            <View style={styles.flatlistItemTextContainer}><Text>{item.referenceStress}</Text></View>
-            <View style={styles.flatlistItemTextContainer}>
-              <FontAwesome5
-                name="arrow-right"
-                size={18}
-                color={'white'}
-                />
-            </View>
-          </Pressable>
-        </Link>
-      )
-
+  const renderAppsHeader = () => {
     return (
-      <TabContainer headerText='Apps'>
-        <Card noPadding>
-          <FlatList
-            data={screenTimeData}
-            renderItem={renderAppsItem}
-            ListHeaderComponent={renderAppsHeader}
-            ListHeaderComponentStyle={styles.flatlistHeaderTextContainer}
-            stickyHeaderIndices={[0]}/>
-        </Card>
-      </TabContainer>
-    );
+      <View style={styles.flatlistItemContainer}>
+        <View style={{ flex: 1, backgroundColor: 'transparent' }}></View>
+        <View style={{ flex: 4, backgroundColor: 'transparent' }}></View>
+        <View style={styles.flatlistHeaderTextContainer}><Text style={{ fontWeight: 'bold' }}>Avg. Stress</Text></View>
+        <View style={styles.flatlistHeaderTextContainer}><Text style={{ fontWeight: 'bold' }}>Ref. Stress</Text></View>
+        <View style={styles.flatlistHeaderTextContainer}></View>
+      </View>
+    )
+  }
 
+  const renderAppsItem = ({ item, index }: { item: any, index: number }) => (
+    <Link href={{ pathname: "/screenTime", params: { date: date.getTime(), image: item.image, name: item.name } }} asChild>
+      <Pressable style={styles.flatlistItemContainer}>
+        <View style={{ flex: 1, backgroundColor: 'transparent', justifyContent: 'center' }}><Image source={item.image} style={styles.imageStyle} /></View>
+        <View style={{ flex: 4, backgroundColor: 'transparent', justifyContent: 'center' }}><Text>{item.name}</Text></View>
+        <View style={styles.flatlistItemTextContainer}><Text>{item.averageStress}</Text></View>
+        <View style={styles.flatlistItemTextContainer}><Text>{item.referenceStress}</Text></View>
+        <View style={styles.flatlistItemTextContainer}>
+          <FontAwesome5
+            name="arrow-right"
+            size={18}
+            color={'white'}
+          />
+        </View>
+      </Pressable>
+    </Link>
+  )
 
-
-
-
-
-
-
-
-
-
+  return (
+    <TabContainer headerText='Apps'>
+      <Card noPadding>
+        <FlatList
+          data={screenTimeData}
+          renderItem={renderAppsItem}
+          ListHeaderComponent={renderAppsHeader}
+          ListHeaderComponentStyle={styles.flatlistHeaderTextContainer}
+          stickyHeaderIndices={[0]} />
+      </Card>
+    </TabContainer>
+  );
 }
 
 
 const styles = StyleSheet.create({
-    flatlistItemContainer: {
-        flexDirection: 'row',
-        flex: 1,
-        backgroundColor: 'transparent',
-        margin: 4,
-        paddingTop: 5,
-        paddingBottom: 5,
-    },
-    flatlistItemTextContainer: {
-        flex: 3,
-        backgroundColor: 'transparent',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    flatlistHeaderTextContainer: {
-        flex: 3,
-        backgroundColor: '#3B3B3B',
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 3,
-        paddingBottom: 3,
-    },
-    containerLabelsContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerLabelsText: {
-        fontSize: 30,
-        padding: 30,
-    },
-    imageStyle: {
-        width: 24,
-        height: 24,
-    },
+  flatlistItemContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: 'transparent',
+    margin: 4,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  flatlistItemTextContainer: {
+    flex: 3,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flatlistHeaderTextContainer: {
+    flex: 3,
+    backgroundColor: '#3B3B3B',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 3,
+    paddingBottom: 3,
+  },
+  containerLabelsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerLabelsText: {
+    fontSize: 30,
+    padding: 30,
+  },
+  imageStyle: {
+    width: 24,
+    height: 24,
+  },
 });
